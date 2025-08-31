d1c0e6c0068665e16fe1d9d7ad1d5d38965c56c5
prathamesh <prathameshjawale068@gmail.com>
2025-08-31 18:29:15 +0530

    added loader

--- frontend/src/components/ChatLoading.js
+++ frontend/src/components/ChatLoading.js
@@ -1,21 +1,19 @@
 import { Stack } from "@chakra-ui/layout";
 import { Skeleton } from "@chakra-ui/skeleton";
+import React from "react";
 
-const ChatLoading = () => {
+const ChatLoading = ({ count = 12 }) => {
   return (
-    <Stack>
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
-      <Skeleton height="45px" />
+    <Stack spacing={3}>
+      {[...Array(count)].map((_, i) => (
+        <Skeleton
+          key={i}
+          height="45px"
+          borderRadius="md"
+          startColor="gray.200"
+          endColor="gray.400"
+        />
+      ))}
     </Stack>
   );
 };
